47932906962513c50d33893a2182aea73963105e
balateja <surlabalateja@gmail.com>
2026-04-21 10:42:56 +0530

    New Branch Commit

---
+++ Wipro_Training/src/Wipro_Assignments/Branch2_test.java
@@ -0,0 +1,10 @@
+package Wipro_Assignments;
+
+public class Branch2_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing branch 2");
+	}
+
+}
