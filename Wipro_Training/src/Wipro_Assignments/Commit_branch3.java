da081d2042bf574260caa9304aea27e2eebfc6b8
balateja <surlabalateja@gmail.com>
2026-04-21 12:06:29 +0530

    just to know weather the branching works properly

---
+++ Wipro_Training/src/Wipro_Assignments/Commit_branch3.java
@@ -0,0 +1,10 @@
+package Wipro_Assignments;
+
+public class Commit_branch3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("just to know wether the branching works properly");
+	}
+
+}
